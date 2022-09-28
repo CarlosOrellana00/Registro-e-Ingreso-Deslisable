86b02f87a8dee16010d4a1dc50077ef51d1db3f5
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2022-09-27 20:39:04 -0400

    JS Funcional

---
+++ effect.js
@@ -0,0 +1,6 @@
+document.getElementById("show-signup-form").addEventListener("click",function(){
+  document.getElementsByClassName("form")[0].classList.add("active");
+});
+document.getElementById("show-signin-form").addEventListener("click",function(){
+  document.getElementsByClassName("form")[0].classList.remove("active");
+});
